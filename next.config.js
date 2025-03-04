fe19321d59c464b23b8a8dd39c3f59e475490925
NgoTomek <ngotomek@gmail.com>
2025-03-04 21:21:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
