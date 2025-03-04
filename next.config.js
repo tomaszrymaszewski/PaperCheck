bc96665537de6fde6e9b532d90d198d4b3bc9cb3
NgoTomek <ngotomek@gmail.com>
2025-03-04 21:14:41 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    // This is needed for PDF.js and other libraries
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    
+    return config;
+  }
+}
+
+module.exports = nextConfig
