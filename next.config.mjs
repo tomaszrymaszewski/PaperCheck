0bf493ed995be9e166d126ce6ed498458feacaf3
NgoTomek <ngotomek@gmail.com>
2025-03-05 12:22:10 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // If you're having issues with CSS not loading, try uncommenting this:
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.css$/,
+  //     use: ["style-loader", "css-loader", "postcss-loader"],
+  //   });
+  //   return config;
+  // },
+};
 
 export default nextConfig;
