e86c8c73319be411970e1890c1a2eb143a35104f
NgoTomek <ngotomek@gmail.com>
2025-03-06 23:11:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-const withTM = require('next-transpile-modules')(['firebase', '@firebase']);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Fix for Firebase auth issues with private class fields
-    config.module.rules.push({
-      test: /node_modules\/undici\/lib\/.*\.js$/,
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['@babel/preset-env', { targets: "defaults" }]
-        ],
-        plugins: [
-          '@babel/plugin-proposal-private-methods',
-          '@babel/plugin-proposal-class-properties'
-        ]
-      }
-    });
-    
-    return config;
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig
