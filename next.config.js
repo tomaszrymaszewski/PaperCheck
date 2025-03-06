e9e4a704f120ee3718da058b8495ae5a2c40901c
NgoTomek <ngotomek@gmail.com>
2025-03-06 23:28:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig
