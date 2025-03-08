c96abb5ff21264d7c481401c1c87af8856a136c0
NgoTomek <ngotomek@gmail.com>
2025-03-08 20:43:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Add this line to generate the standalone output
   output: 'standalone'
 };
 
