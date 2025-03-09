3e6e3f4c386b023e89080d73a1b970ab4a9bd8c3
NgoTomek <ngotomek@gmail.com>
2025-03-09 21:23:17 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-900 text-white`}>
+      <body className={`${inter.className} bg-gray-900`}>
         <AuthProvider>
           {children}
         </AuthProvider>
